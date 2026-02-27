72b1caa7398b29956235f96a8f7b9a587498a1f6
Muharem Huskić <muharemhuskic955@gmail.com>
2026-02-28 00:42:47 +0100

    app/models/friendship.rb:
    class Friendship < ApplicationRecord
      belongs_to :user
      belongs_to :friend, class_name: 'User'
    
      validates :user_id, uniqueness: { scope: :friend_id }
      validate :cannot_add_self
    
      private
    
      def cannot_add_self
        errors.add(:friend_id, "can't add yourself") if user_id == friend_id
      end
    end

---
+++ friendship model.rb
@@ -0,0 +1,13 @@
+class Friendship < ApplicationRecord
+  belongs_to :user
+  belongs_to :friend, class_name: 'User'
+
+  validates :user_id, uniqueness: { scope: :friend_id }
+  validate :cannot_add_self
+
+  private
+
+  def cannot_add_self
+    errors.add(:friend_id, "can't add yourself") if user_id == friend_id
+  end
+end
