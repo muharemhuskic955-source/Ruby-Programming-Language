c044b8e286a648d0d6ec40d2548c074c030552e0
Muharem Huskić <muharemhuskic955@gmail.com>
2026-02-28 00:50:21 +0100

    config/routes.rb:
    Rails.application.routes.draw do
      devise_for :users
      root 'posts#index'
    
      resources :posts do
        resources :comments
      end
    
      resources :users, only: [:index, :show]
      
      resources :friendships, only: [:create, :destroy]
    end

---
+++ Routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'posts#index'
+
+  resources :posts do
+    resources :comments
+  end
+
+  resources :users, only: [:index, :show]
+  
+  resources :friendships, only: [:create, :destroy]
+end
